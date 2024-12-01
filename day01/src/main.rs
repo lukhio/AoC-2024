b269857a555e46c629e63b9781791007a0fed874
Julien Gamba <julien@jgamba.eu>
2024-12-01 22:02:42 +0100

    day 01 - part 2

--- day01/src/main.rs
+++ day01/src/main.rs
@@ -5,6 +5,8 @@ use std::io::{ BufRead, BufReader };
 fn main() {
     let distance = part_one().unwrap();
     println!("Part one distance: {distance}");
+    let score = part_two().unwrap();
+    println!("Part two similarity score: {score}");
 }
 
 fn part_one() -> Result<u32, &'static str> {
@@ -41,4 +43,40 @@ fn part_one() -> Result<u32, &'static str> {
     }
 
     Ok(distance)
+}
+
+fn part_two() -> Result<u32, &'static str> {
+    let input = File::open("./input")
+                           .expect("cannot open input file");
+    let reader = BufReader::new(input);
+
+    // firsts ints
+    let mut firsts = Vec::new();
+    // seconds ints
+    let mut seconds = Vec::new();
+
+    for line in reader.lines() {
+        // split the line at the first space
+        let (first, second) = line.as_ref()
+            .expect("Cannot get line ref")
+            .split_once(' ')
+            .expect("Cannot split line");
+
+        firsts.push(first.parse::<u32>()
+                         .expect("Cannot convert {first} to u32"));
+        seconds.push(second.trim().parse::<u32>()
+                           .expect("Cannot convert {second} to u32"));
+    }
+
+    // sort lists
+    firsts.sort();
+    seconds.sort();
+
+    // loop through both lists and compute distance
+    let mut score = 0;
+    for first in firsts.into_iter() {
+        score += first * seconds.iter().filter(|&second| *second == first).count() as u32;
+    }
+
+    Ok(score)
 }
