a1fd2be562e49b78ac4dbe7d94fe9f53604d7256
Julien Gamba <julien@jgamba.eu>
2024-12-04 18:41:59 +0100

    day 02 cleanup

--- day02/src/main.rs
+++ day02/src/main.rs
@@ -8,36 +8,6 @@ fn main() {
     println!("Number of safe reports: {safe_count:?}");
 }
 
-fn part_one() -> Result<u32, String> {
-    let input = File::open("input")
-                           .expect("cannot open input file");
-    let reader = BufReader::new(input);
-
-    let mut safe_reports = 0;
-    for line in reader.lines() {
-        let line = line.expect("Error: cannot read line from input reader");
-
-        let elements = line.split_whitespace()
-            .map(|i| i.parse::<u32>().expect("Error: cannot convert level to u32"))
-            .collect::<Vec<u32>>();
-
-        // check all ascending and absolute difference between 1 and 3
-        if elements.windows(2)
-                   .all(|w| (w[0] < w[1] && w[0].abs_diff(w[1]) >= 1 && w[0].abs_diff(w[1]) <= 3)) {
-            safe_reports += 1;
-            continue;
-        }
-
-        // check all descending and absolute difference between 1 and 3
-        if elements.windows(2)
-                   .all(|w| (w[0] > w[1] && w[0].abs_diff(w[1]) >= 1 && w[0].abs_diff(w[1]) <= 3)) {
-            safe_reports += 1;
-        }
-    }
-
-    Ok(safe_reports)
-}
-
 fn is_increasing(array: &[u32]) -> bool {
     array.windows(2).all(|w| w[0] < w[1])
 }
@@ -52,6 +22,27 @@ fn check_diff(array: &[u32]) -> bool {
 
 fn is_safe(array: &[u32]) -> bool {
     check_diff(array) && (is_increasing(array) || is_decreasing(array))
+}
+
+fn part_one() -> Result<u32, String> {
+    let input = File::open("input")
+                           .expect("cannot open input file");
+    let reader = BufReader::new(input);
+
+    let mut safe_reports = 0;
+    for line in reader.lines() {
+        let line = line.expect("Error: cannot read line from input reader");
+
+        let elements = line.split_whitespace()
+            .map(|i| i.parse::<u32>().expect("Error: cannot convert level to u32"))
+            .collect::<Vec<u32>>();
+
+        if is_safe(&elements) {
+            safe_reports += 1;
+        }
+    }
+
+    Ok(safe_reports)
 }
 
 fn part_two() -> Result<u32, String> {
